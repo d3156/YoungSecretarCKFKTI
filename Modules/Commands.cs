2e600259503eb4b790458918d442df9e78c9ef91
Ярослав Смирнов <d3156@mail.ru>
2021-01-25 13:57:21 +0300

    Первые шаги

---
+++ Modules/Commands.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace YoungSecretarCKFKTI.Modules
+{
+
+    static class Links
+    {
+        private static List<string> LinksArray;
+        public static string Link { 
+            set 
+            { 
+                if (LinksArray == null)
+                {
+                    LinksArray = new List<string>();
+                    if (File.Exists("Links.txt")) LinksArray.AddRange(File.ReadAllLines("Links.txt"));
+                }
+                LinksArray.Add(value);
+                if (File.Exists("Links.txt")) File.Delete("Links.txt");
+                File.WriteAllLines("Links.txt", LinksArray);
+            } 
+        }
+
+        public static List<string> GetLinks
+        {
+            get
+            {
+                if (LinksArray == null)
+                {
+                    LinksArray = new List<string>();
+                    if (File.Exists("Links.txt")) LinksArray.AddRange(File.ReadAllLines("Links.txt"));
+                }
+                return LinksArray;
+            }
+        }
+    }
+
+
+    public class Commands : ModuleBase<SocketCommandContext>            
+    {
+        [Command("удачного дня")]
+        public async Task GoodDay()=> await ReplyAsync("И тебе удачного дня, товарищ!");
+        [Command("добавьте ссылку в список")]
+        public async Task AddLink(string Link = null)
+        {
+            if (Link == null) { await ReplyAsync("Выражайтесь корректнее, пожалуйста"); return; }
+            Links.Link = Link;
+            await ReplyAsync("Добавлено, товарищи!");
+        }
+
+        [Command("предоставьте список ссылок")]
+        public async Task ShowLink()
+        {
+            string message = "Вот выписка из архива:\n";
+            foreach (var item in Links.GetLinks) message += item + "\n";
+            await ReplyAsync(message);
+        }
+
+        [Command("спасибо")] public async Task Thancks() => await ReplyAsync("Я просто делаю свою работу, товарищ!");
+        [Command("помощь")] public async Task Help() => await ReplyAsync("удачного дня - пожелать удачного дня" +
+            "\nдобавьте ссылку в список - добавление интернет-ссылки и иной информации в партийный архив" +
+            "\nпредоставьте список ссылок - выписка из архива" +
+            "\nспасибо - поблагодарить секретаря за помощь" +
+            "\nнайти в архиве - поиск записи в архиве");
+        [Command("найти в архиве")]
+        public async Task FindLink(string Link = null)
+        {
+            if (Link == null) { await ReplyAsync("Выражайтесь корректнее, пожалуйста"); return; }
+            var arhive = Links.GetLinks;
+            var finded = arhive.FindAll(x => x.Contains(Link));
+            string message = "Вот что удалось найти в архиве:\n";
+            foreach (var item in finded) message += item + "\n";
+            await ReplyAsync(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YoungSecretarCKFKTI
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+            string Token = "ODAzMTc3NzQ3NjQyOTc0MjM4.YA5_kQ.v0lIV9VkE5sGnWafhAD7VhGhC9M";
+
+            _client.Log += Log;
+            await RegisterComandAsync();
+            await _client.LoginAsync(TokenType.Bot, Token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.Message);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterComandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        public async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("Товарищ секретарь, ",ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+
+            }
+        }
+    }
+}
